511676ae84d2c8531768174e11027cc3dbf0ad4d
OksanaBakl <oksanamolesha@ukr.net>
2021-11-03 01:01:07 +0200

    fix

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,8 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createAction } from '@reduxjs/toolkit';
 
-uuidv4();
-
 const addContact = createAction('contacts/add', ({ name, number }) => ({
   payload: {
     id: uuidv4(),
